33e953237e08c9fc662b5ba2e0685b02f0d9761b
DanielPa <papp.technik@gmail.com>
2017-02-16 19:09:13 +0100

    Add files via upload

---
+++ OpenProjectPath/OpenProjectPath.ps1
@@ -0,0 +1,5 @@
+﻿$projectPath = "C:\WSCAD\WSCAD SUITE\2017\Projects\"
+$searchPattern = "PRJDATA6.ldb"
+$filesFound = [System.IO.Directory]::EnumerateFiles($projectPath,$searchPattern,"AllDirectories")
+$pathToOpen = [System.IO.Path]::GetDirectoryName($filesFound[0])
+ii $pathToOpen
